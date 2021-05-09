15fccc23e2b30dd1b1cdb136740eb6db0a6bddb5
亀茶 <masakikameyama81@gmail.com>
2021-05-09 23:52:59 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by masaki kameyama on 2021/05/09.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
